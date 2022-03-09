4c09482b10338fc0f98aa7456cc7dc1dad0d93d1
Nicolas de Guilhem de Lataillade <nicolas.dgdl@hotmail.fr>
2022-03-09 20:00:59 +0100

    working advice generator

--- fetch.js
+++ fetch.js
@@ -1,9 +1,13 @@
-const apiUrl = "https://api.adviceslip.com/advice"
+const fetchApi = () => {
+  const apiUrl = "https://api.adviceslip.com/advice"
+  fetch(apiUrl).then(response => response.json())
+    .then((data) => {
+      document.getElementById("advice-content").innerHTML = data.slip.advice;
+      document.getElementById("advice-id").innerHTML = data.slip.id;
+    })}
 
 window.addEventListener('load', function() {
-  fetch(apiUrl).then(response => response.json())
-                .then((data) => {
-                  document.getElementById("advice-content").innerHTML = data.slip.advice
-                  document.getElementById("advice-id").innerHTML = data.slip.id
-                });
+  fetchApi()
+  const button = this.document.getElementById("shuffle")
+  button.addEventListener('click', function() { fetchApi() })
 })

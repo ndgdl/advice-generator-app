16a48284965c755c3521626791adc28790fe77ab
Nicolas de Guilhem de Lataillade <nicolas.dgdl@hotmail.fr>
2022-03-10 17:04:50 +0100

    added spinning dice

--- fetch.js
+++ fetch.js
@@ -1,5 +1,6 @@
 const fetchApi = () => {
   const apiUrl = "https://api.adviceslip.com/advice"
+
   fetch(apiUrl).then(response => response.json())
     .then((data) => {
       document.getElementById("advice-content").innerHTML = data.slip.advice;
@@ -10,5 +11,13 @@ const fetchApi = () => {
 window.addEventListener('load', function() {
   fetchApi();
   const button = this.document.getElementById("shuffle");
-  button.addEventListener('click', function() { fetchApi() })
+  button.addEventListener('click', function() {
+    const dice = document.getElementById("dice");
+
+    fetchApi();
+    dice.classList.add("spin");
+    setTimeout(() => {
+      dice.classList.remove("spin");
+    }, 1000);
+  })
 })
